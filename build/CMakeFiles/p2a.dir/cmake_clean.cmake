3d79b7bf2373ed3b49bf555b8119af65d07f0614
Kalea Louie <kalealouie@yahoo.com>
2017-01-29 16:45:24 -0800

    Initial commit

---
+++ build/CMakeFiles/p2a.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p2a.dir/src/GLSL.cpp.o"
+  "CMakeFiles/p2a.dir/src/MatrixStack.cpp.o"
+  "CMakeFiles/p2a.dir/src/Program.cpp.o"
+  "CMakeFiles/p2a.dir/src/main.cpp.o"
+  "p2a.pdb"
+  "p2a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p2a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
